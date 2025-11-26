5033de83ac7f7ddeb473a7b97f065eacf11d79ee
RushilK7 <rushilk@lambdatest.com>
2025-11-26 15:44:33 +0530

    Update smartui-web.json to match CLI default config structure

--- sdk/sdkLocal.js
+++ sdk/sdkLocal.js
@@ -6,9 +6,7 @@ const { smartuiSnapshot } = require('@lambdatest/selenium-driver');
 
   try {
     await driver.get("https://www.lambdatest.com");
-    await smartuiSnapshot(driver, "Lambdatest");
-    await driver.get("https://www.pinterest.com/pin/112801165652823604/");
-    await smartuiSnapshot(driver, "NYC");
+    await smartuiSnapshot(driver, "screenshot");
   } finally {
     await driver.quit();
   }
